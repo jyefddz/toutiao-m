e68f60a0754b70ba779998c91627abae0bb84fd4
jyefddz <1065697146@qq.com>
2022-07-13 12:05:36 +0800

    完成项目初始化

---
+++ .postcssrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: {
+    'postcss-pxtorem': {
+      // 设计稿1rem的大小
+      // vant组件库是根据37.5
+      // 动态设置rootValue
+      // rootValue: (module) => {
+      // 如果是vant组件 就返回37.5
+      // 不是就返回75
+      // if (/vant/gi.test(module.file)) {
+      // return 37.5
+      // } else {
+      // return 75
+      // }
+      // },
+      rootValue: (module) => (/vant/gi.test(module.file) ? 37.5 : 75),
+
+      //   适配的属性
+      propList: ['*']
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,10 +3,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入字体图标css
+import '@/assets/fonts/iconfont.css'
+
+// 引入vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 引入flexble
+import 'amfe-flexible/index.min.js'
+
 Vue.config.productionTip = false
+
+Vue.use(Vant)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
