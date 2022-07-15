ca748d8276a066f301ddbb3fe9e4d357fa880e15
jyefddz <1065697146@qq.com>
2022-07-15 17:53:12 +0800

    完成布局页面路由的配置

---
+++ src/views/Layout/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    布局页面
+    <router-view></router-view>
+    <van-tabbar route>
+      <van-tabbar-item to="/ ">
+        <template #icon>
+          <span class="toutiao toutiao-shouye"></span>
+          <span class="text">首页</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/video">
+        <template #icon>
+          <span class="toutiao toutiao-shipin"></span>
+          <span class="text">视频</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/qa">
+        <template #icon>
+          <span class="toutiao toutiao-wenda"></span>
+          <span class="text">问答</span>
+        </template>
+      </van-tabbar-item>
+      <van-tabbar-item to="/profile">
+        <template #icon>
+          <span class="toutiao toutiao-wode"></span>
+          <span class="text">我的</span>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+// route 路由模式是什么意思
+// 1.每一个item 绑定了to属性
+// 2.route 模式, 就是拿到path以后,和to属性里面的值比对.比对成功,就高亮
+export default {}
+</script>
+
+<style scoped lang="less">
+:deep(.van-tabbar-item__icon) {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: space-evenly;
+  height: 100%;
+  .toutiao {
+    font-size: 0.53333rem;
+  }
+  .text {
+    font-size: 0.32rem;
+  }
+}
+</style>
